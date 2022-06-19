18a2a5e76e282efef7c2f7b135a50288ad0f253f
baxelson12 <bradaxelson@gmail.com>
2022-06-19 08:47:53 -0400

    feat: Implement module config

--- projects/ngx-paperxyz-sdk/src/lib/ngx-paperxyz-sdk.module.ts
+++ projects/ngx-paperxyz-sdk/src/lib/ngx-paperxyz-sdk.module.ts
@@ -1,14 +1,33 @@
-import { NgModule } from '@angular/core';
+import { Inject, ModuleWithProviders, NgModule } from '@angular/core';
 import { PaperCreateWalletComponent } from './components/create-wallet.component';
 import { PaperPayWithCardComponent } from './components/pay-with-card.component';
 import { PaperCreateWalletService } from './services/create-wallet.service';
 import { PaperEventsHandlerService } from './services/events-handler.service';
 import { PaperPayWithCardService } from './services/pay-with-card.service';
+import { PaperModuleConfig, PAPER_CONFIG_TOKEN } from './tokens/config.token';
+import { hasKeys } from './utils/has-keys';
 
 @NgModule({
   declarations: [],
   imports: [PaperCreateWalletComponent, PaperPayWithCardComponent],
-  providers: [PaperEventsHandlerService, PaperCreateWalletService, PaperPayWithCardService],
+  providers: [
+    PaperEventsHandlerService,
+    PaperCreateWalletService,
+    PaperPayWithCardService,
+    { provide: PAPER_CONFIG_TOKEN, useValue: {} },
+  ],
   exports: [PaperCreateWalletComponent, PaperPayWithCardComponent],
 })
-export class NgxPaperXYZSdk {}
+export class NgxPaperXYZSdk {
+  public static forRoot(opts: PaperModuleConfig): ModuleWithProviders<NgxPaperXYZSdk> {
+    return {
+      ngModule: NgxPaperXYZSdk,
+      providers: [{ provide: PAPER_CONFIG_TOKEN, useValue: opts }],
+    };
+  }
+
+  constructor(@Inject(PAPER_CONFIG_TOKEN) config: PaperModuleConfig) {
+    !hasKeys(config, 'chainName', 'chainName') &&
+      console.error('Paper: forRoot config object is missing properties!');
+  }
+}
